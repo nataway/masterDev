20752bed92917f14fd142c886871a965077648be
nataway <buiminhchi0405@gmail.com>
2022-06-07 17:17:37 +0700

    bai3

---
+++ bai3/avsc_demo/src/main/java/Deserialize.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.specific.SpecificDatumReader;
+import with_code_gen.UserInfo;
+
+public class Deserialize {
+    public static void main(String args[]) throws IOException {
+
+        //DeSerializing the objects
+        DatumReader<UserInfo> empDatumReader = new SpecificDatumReader<>(UserInfo.class);
+
+        Path currentRelativePath = Paths.get("");
+        String s = currentRelativePath.toAbsolutePath().toString();
+        //Instantiating DataFileReader
+        DataFileReader<UserInfo> dataFileReader = new DataFileReader<>(new
+                File(s+"/src/main/java/with_code_gen/emp.avro"), empDatumReader);
+        UserInfo em=null;
+
+        while(dataFileReader.hasNext()){
+            em=dataFileReader.next(em);
+            System.out.println(em);
+        }
+    }
+}
---
+++ bai3/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+            SpringApplication.run(DemoApplication.class, args);
+
+    }
+
+
+}
