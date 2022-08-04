d47b57f5c292ffad3bedfe70366d87fe0adc8fa6
nataway <buiminhchi0405@gmail.com>
2022-08-04 19:01:29 +0700

    elasticsearch

---
+++ bai11_Elasticsearch/elasticsearch/src/main/java/com/example/elasticsearch/Model/SearchField.java
@@ -0,0 +1,8 @@
+package com.example.elasticsearch.Model;
+
+public class SearchField {
+    public static String FIELD_NAME = "suggest_title";
+    public static String NAME_SUGGESTION = "title-suggest";
+    public static String INDICES = "title_suggest_chibm";
+
+}
